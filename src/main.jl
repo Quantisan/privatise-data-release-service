299a4aa38ac8fac67b17806c347da3db5ea859b4
Paul Lam <paul@quantisan.com>
2015-06-04 11:27:29 -0700

    slap on a fixed dataset to MWEM and print to a route

--- src/main.jl
+++ src/main.jl
@@ -1,9 +1,15 @@
 using HttpServer
+using PrivateMultiplicativeWeights
 
 # Define request handling
 function mainhandler(req, res)
-    if req.resource == "/home"
-        res.data = "Hello world!"
+    if req.resource == "/do"
+        d, n = 20, 1000
+        data_matrix = rand(0:1,d,n)
+        data_matrix[3,:] = data_matrix[1,:] .* data_matrix[2,:]
+        mw = mwem(Parities(d,3),Tabular(data_matrix))
+        table = Tabular(mw.synthetic, n)
+        res.data = string(table)
     else
         res.status = 404
         res.data   = "Requested resource not found"
